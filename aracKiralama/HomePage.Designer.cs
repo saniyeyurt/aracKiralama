bd8a05683b3e88efb9e35e7fbdc71dfa6a613daa
Saniye <Saniye@DESKTOP-5I7N1N2>
2022-08-06 21:13:07 +0300

    Add project files.

---
+++ aracKiralama/HomePage.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace aracKiralama
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(733, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.ImageIndex = 4;
+            this.button1.ImageList = this.ımageList1;
+            this.button1.Location = new System.Drawing.Point(14, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Müşteri Ekleme";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.ImageIndex = 0;
+            this.button2.ImageList = this.ımageList1;
+            this.button2.Location = new System.Drawing.Point(309, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 76);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Araç Listeleme";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.ImageIndex = 1;
+            this.button3.ImageList = this.ımageList1;
+            this.button3.Location = new System.Drawing.Point(211, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 76);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Araç Kayıt";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.ImageIndex = 3;
+            this.button4.ImageList = this.ımageList1;
+            this.button4.Location = new System.Drawing.Point(115, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 76);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Müşteri Listeleme";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button5.ImageIndex = 6;
+            this.button5.ImageList = this.ımageList1;
+            this.button5.Location = new System.Drawing.Point(404, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 76);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Sözleşme";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button6.ImageList = this.ımageList1;
+            this.button6.Location = new System.Drawing.Point(504, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 76);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Satışlar";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button7.ImageIndex = 2;
+            this.button7.ImageList = this.ımageList1;
+            this.button7.Location = new System.Drawing.Point(612, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 76);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Çıkış";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "araç kayıt.png");
+            this.ımageList1.Images.SetKeyName(1, "araç kayıtt.png");
+            this.ımageList1.Images.SetKeyName(2, "çıkış.png");
+            this.ımageList1.Images.SetKeyName(3, "müşteri liste.PNG");
+            this.ımageList1.Images.SetKeyName(4, "müşteriekle.jpg");
+            this.ımageList1.Images.SetKeyName(5, "satışlar.png");
+            this.ımageList1.Images.SetKeyName(6, "sözleşme.png");
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = global::aracKiralama.Properties.Resources.homepagee;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(733, 541);
+            this.Controls.Add(this.panel1);
+            this.Name = "HomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HomePage";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
